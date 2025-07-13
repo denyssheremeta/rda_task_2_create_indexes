d8310eb695c2c821fff9c594ff9bc5c5fcbf6a87
Denys Sheremeta <DSheremeta@franpos.com>
2025-07-13 12:24:05 +0300

    Solution1

--- task.sql
+++ task.sql
@@ -1,8 +1,4 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
-
-CREATE INDEX Email ON Customers (Email);
-
-CREATE INDEX WarehouseAmount ON Products (WarehouseAmount);
-
-CREATE INDEX Name ON Products (Name);
+CREATE INDEX Email ON Customers(Email);
+CREATE INDEX Name ON Products(Name);
